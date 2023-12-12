be0935c4ffb9d5eeffc1e807df6c02392aec915c
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2023-12-13 01:36:10 +0200

    Homework 1. Д.З. Реалізувати у вигляді функції із запуском з основного модулю Забезпечити введеня двох різних додатніх чисел та розрахунок їх суми. Якщо вводяться від'ємні числа або однакові, то пропонується зробити повторне введеня чисел: Введіть x = -1 Потрібне додатнє число Введіть x = 2 Введіть y = -5 Потрібне додатнє число Введіть y = 2 Числа повинні відрізнятись Введіть y = 3 2 + 3 = 5

--- Intro/functions.py
+++ Intro/functions.py
@@ -13,10 +13,38 @@ def printPair(x, y):
     print(str)
 
 
+def homework():
+    val1 = 0
+    val2 = 0
+    isValid = False
+    while True:
+        if isValid == False:
+            val1 = input("Введіть х: ")
+            val1 = int(val1)
+            if val1 < 0:
+                print("Потрібно додатнє число")
+                continue
+            else:
+                isValid=True
+                
+        val2 = input("Введіть y: ")
+        val2 = int(val2)
+        if val2 < 0:
+            print("Потрібно додатнє число")
+            continue
+        elif val2 == val1:
+            print("Числа повинні відрізнятись")
+            continue
+        else:
+            break
+    print("%d + %d = %d" % (val1, val2, val1+val2))
+
 def main() -> None:
     print(hello())
     x,y = pair()
     printPair(x, y)
+    homework()
+    
 
 # Одне з використань функцій - створення "точки входження"
 # це корисне для впровадження інжектування, а також для
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Server-Python201
- Learn server Python
+# Server-Python201 Homeworks
+
+## Homework 1 12.12
+![alt](https://github.com/dsgnrr/Server-Python201/blob/main/Homework/hw1/result.png)
