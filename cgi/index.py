ddf3e04518dcf326b692d60ad6e0750c45d9fc96
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2023-12-18 13:12:37 +0200

    Lesson 4. Common Gateway Interface

---
+++ cgi/index.py
@@ -0,0 +1,15 @@
+#!D:/Programs/Python3/python.exe
+
+import os
+
+print("Content-Type: text/html")
+print("") # заголовки від тіла відокремлюються порожнім рядком
+print(f"""<!doctype html>
+<html>
+    <head>
+      <title>Py-201</title>
+    </head>
+    <body>
+      <h1>CGI працює</h1>
+    </body>
+</html>""")
